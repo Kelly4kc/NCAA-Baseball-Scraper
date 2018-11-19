019cbd53267348ff46ee4d18fefbf56471754e8b
Terri <Terri@MomLaptop2015.fios-router.home>
2018-11-19 10:04:35 -0500

    Fix Scrape runner

--- src/NCAAScrapeRunner.java
+++ src/NCAAScrapeRunner.java
@@ -9,21 +9,17 @@ import utils.Type;
 public class NCAAScrapeRunner {
   public static void main(String[] args) {
     long startTime = System.currentTimeMillis();
-    /*
-     * for (int year = 2018; year < 2019; year++) { NCAATeamScraper.scrapeTeams(year, 1);
-     * NCAAConferenceScraper.scrapeConferences(year, 1);
-     * NCAATeamScraper.scrapeTeamsByConference(year, 1); }
-     */
-   // for (int year = 2012; year < 2018; year++) {
-     // NCAAScheduleScraper.scrapeSchedule(year, 1);
-    //}
-    //NCAABoxScoreScraper.scrapeGameInfo(2018, 1);
-   // for (Type t : Type.values()) {
-    //  NCAABoxScoreScraper.scrapeBoxScores(2018, 1, t);
-    //}
-    
-    //NCAAPlayByPlayScraper.scrapePlayByPlay(2018, 1);
-    
+    for (int year = 2012; year < 2018; year++) {
+      // NCAATeamScraper.scrapeTeams(year, 1);
+      // NCAAConferenceScraper.scrapeConferences(year, 1);
+      // NCAATeamScraper.scrapeTeamsByConference(year, 1);
+      // NCAAScheduleScraper.scrapeSchedule(year, 1);
+      // NCAABoxScoreScraper.scrapeGameInfo(year, 1);
+      for (Type t : Type.values()) {
+        // NCAABoxScoreScraper.scrapeBoxScores(year, 1, t);
+      }
+      // NCAAPlayByPlayScraper.scrapePlayByPlay(year, 1);
+    }
     long endTime = System.currentTimeMillis();
     System.out.println("Total Time: " + (endTime - startTime) / 1000.0);
   }
